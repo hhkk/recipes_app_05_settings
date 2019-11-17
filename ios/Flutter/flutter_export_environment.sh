1c5449468f3c3ece41f165477473f2fd6f408e4b
hhkk <hank.khang@gmail.com>
2019-11-17 03:50:02 -0500

    d

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/hkon/dev/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/hkon/utd/a191020FlutterRecipe/recipes_app_05_settings"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/hkon/dev/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
